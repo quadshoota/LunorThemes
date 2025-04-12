070a76c80252185043ada6eef4201b0991ac6cc8
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-04-12 18:31:44 +0200

    Update ReturnedThemes.lua

--- ReturnedThemes.lua
+++ ReturnedThemes.lua
@@ -1,5 +1,5 @@
 local themes = {
-	original = {
+	default = {
 		maincolor = Color3.fromRGB(9, 9, 9),
 		bordercolor = Color3.fromRGB(39, 39, 42),
 		scrollocolor = Color3.fromRGB(28, 28, 30),
@@ -40,50 +40,100 @@ local themes = {
 
 		valuetext = Color3.fromRGB(255, 255, 255),
 		valuebg = Color3.fromRGB(39, 39, 42),
+
+		-- Bind
+		-- not need ah sigma pro sex master
 	},
-	
-	default = {
-		maincolor = Color3.fromRGB(15, 15, 15),
+	Christmas = {
+		maincolor = Color3.new(1.000000, 0.623529, 0.623529),
 		bordercolor = Color3.fromRGB(39, 39, 42),
-		scrollocolor = Color3.fromRGB(30, 28, 28),
+		scrollocolor = Color3.fromRGB(28, 28, 30),
 
-		titlecolor = Color3.fromRGB(245, 245, 245),
-		descriptioncolor = Color3.fromRGB(180, 180, 180),
-		elementdescription = Color3.fromRGB(180, 180, 180),
+		titlecolor = Color3.new(1.000000, 1.000000, 1.000000),
+		descriptioncolor = Color3.new(1.000000, 1.000000, 1.000000),
+		elementdescription = Color3.fromRGB(168, 168, 168),
 
-		primarycolor = Color3.fromRGB(240, 96, 96),
+		primarycolor = Color3.new(0.854902, 0.415686, 0.415686),
 
 		-- Tabs Themes
-		offTextBtn = Color3.fromRGB(80, 80, 80),
-		offBgLineBtn = Color3.fromRGB(30, 30, 30),
-		onTextBtn = Color3.fromRGB(240, 96, 96),
-		onBgLineBtn = Color3.fromRGB(240, 96, 96),
+		offTextBtn = Color3.new(0.878431, 0.878431, 0.878431),
+		offBgLineBtn = Color3.new(0.839216, 0.839216, 0.839216),
+		onTextBtn = Color3.new(0.858824, 0.356863, 0.356863),
+		onBgLineBtn = Color3.new(0.858824, 0.356863, 0.356863),
 
 		--#elementsthemes
 
 		-- Toggle
-		toggleborder = Color3.fromRGB(240, 96, 96),
-		togglebg = Color3.fromRGB(186, 58, 58),
+		toggleborder = Color3.new(1.000000, 1.000000, 1.000000),
+		togglebg = Color3.new(0.921569, 0.203922, 0.203922),
 
 		-- Slider
-		sliderbar = Color3.fromRGB(21, 21, 21),
-		sliderbarstroke = Color3.fromRGB(35, 35, 35),
-		sliderprogressbg = Color3.fromRGB(186, 58, 58),
-		sliderprogressborder = Color3.fromRGB(240, 96, 96),
-		sliderdotbg = Color3.fromRGB(240, 96, 96),
-		sliderdotstroke = Color3.fromRGB(15, 15, 15),
+		sliderbar = Color3.fromRGB(21,21,21),
+		sliderbarstroke = Color3.fromRGB(29, 29, 29),
+		sliderprogressbg = Color3.new(0.921569, 0.152941, 0.152941),
+		sliderprogressborder = Color3.new(1.000000, 0.196078, 0.141176),
+		sliderdotbg = Color3.new(1.000000, 0.262745, 0.262745),
+		sliderdotstroke = Color3.fromRGB(9, 9, 9),
 
 		-- Dropdown
-		containeritemsbg = Color3.fromRGB(25, 25, 25),
+		containeritemsbg = Color3.new(1.000000, 0.121569, 0.121569),
 
-		itembg = Color3.fromRGB(39, 39, 42),
-		itemcheckmarkcolor = Color3.fromRGB(240, 96, 96),
-		itemTextOn = Color3.fromHex("#ffffff"),
-		itemTextOff = Color3.fromHex("#9a9a9a"),
+		itembg = Color3.new(0.933333, 0.188235, 0.188235),
+		itemcheckmarkcolor = Color3.fromRGB(154, 154, 154),
+		itemTextOn = Color3.fromHex("#e9e9e9"),
+		itemTextOff = Color3.fromHex("9a9a9a"),
 
 		valuetext = Color3.fromRGB(255, 255, 255),
-		valuebg = Color3.fromRGB(40, 40, 43),
+		valuebg = Color3.fromRGB(39, 39, 42),
+
+		-- Bind
+		-- not need ah sigma pro sex master
 	},
+    	BlueLockRivals = {
+                maincolor = Color3.fromRGB(9, 9, 9),
+                bordercolor = Color3.fromRGB(39, 39, 42),
+                scrollocolor = Color3.fromRGB(28, 28, 30),
+        
+                titlecolor = Color3.fromRGB(245, 245, 245),
+                descriptioncolor = Color3.fromRGB(168, 168, 168),
+                elementdescription = Color3.fromRGB(168, 168, 168),
+        
+                primarycolor = Color3.fromRGB(3, 181, 252),
+        
+                -- Tabs Themes
+                offTextBtn = Color3.fromRGB(63, 63, 63),
+                offBgLineBtn = Color3.fromRGB(29, 29, 29),
+                onTextBtn = Color3.fromRGB(3, 252, 248),
+                onBgLineBtn = Color3.fromRGB(3, 252, 248),
+        
+                --#elementsthemes
+        
+                -- Toggle
+                toggleborder = Color3.fromRGB(3, 252, 248),
+                togglebg = Color3.fromRGB(3, 181, 252),
+        
+                -- Slider
+                sliderbar = Color3.fromRGB(21,21,21),
+                sliderbarstroke = Color3.fromRGB(29, 29, 29),
+                sliderprogressbg = Color3.fromRGB(130, 255, 249),
+                sliderprogressborder = Color3.fromRGB(3, 252, 248),
+                sliderdotbg = Color3.fromRGB(3, 57, 252),
+                sliderdotstroke = Color3.fromRGB(9, 9, 9),
+        
+                -- Dropdown
+                containeritemsbg = Color3.fromRGB(28, 25, 23),
+        
+                itembg = Color3.fromRGB(39, 39, 42),
+                itemcheckmarkcolor = Color3.fromRGB(154, 154, 154),
+                itemTextOn = Color3.fromHex("#e9e9e9"),
+                itemTextOff = Color3.fromHex("9a9a9a"),
+        
+                valuetext = Color3.fromRGB(255, 255, 255),
+                valuebg = Color3.fromRGB(39, 39, 42),
+        
+                -- Bind
+                -- not need ah sigma pro sex master
+            },
 }
 
 return themes
